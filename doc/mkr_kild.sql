949836dc20f74b16ad22484aa51979a2e3f152e1
Rete-Keyt Uibo <Rete-Keyt.Uibo@reach-u.com>
2017-05-09 20:03:10 +0300

    database dump

--- doc/mkr_kild.sql
+++ doc/mkr_kild.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Mai 08, 2017 kell 09:07 AM
--- Serveri versioon: 10.1.16-MariaDB
--- PHP versioon: 5.6.24
+-- Loomise aeg: Mai 09, 2017 kell 07:02 PL
+-- Serveri versioon: 10.1.21-MariaDB
+-- PHP versioon: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,11 +23,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `products`
+-- Tabeli struktuur tabelile `product`
 --
 
-CREATE TABLE `products` (
-  `product_id` int(10) UNSIGNED NOT NULL COMMENT 'Autocreated',
+CREATE TABLE `product` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT 'Autocreated',
   `product_name` varchar(50) NOT NULL COMMENT 'Autocreated',
   `product_type_id` tinyint(4) NOT NULL DEFAULT '1',
   `product_image` varchar(191) NOT NULL,
@@ -37,33 +37,53 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Andmete tõmmistamine tabelile `products`
+-- Andmete tõmmistamine tabelile `product`
 --
 
-INSERT INTO product (id, `product_name`, `product_type_id`, `product_image`, `product_price`, `product_qty`, `product_details`) VALUES
-(1, 'C', 1, 'kapid.jpg', '212.00', 19, 'Ilus asi'),
-(2, 'furniture #2', 2, 'laud.jpg', '123.00', 8, 'teine asi'),
-(3, 'Mingi asi', 3, 'Riiul1.jpg', '1231.00', 4, 'kolmas käib kah'),
+INSERT INTO `product` (`id`, `product_name`, `product_type_id`, `product_image`, `product_price`, `product_qty`, `product_details`) VALUES
+(2, 'furniture #2', 1, 'laud.jpg', '123.00', 8, 'teine asi'),
+(3, 'Mingi asi', 1, 'Riiul1.jpg', '1231.00', 4, 'kolmas käib kah'),
 (5, 'Araablane', 2, 'voodi.jpg', '9.99', 970, 'Osta kohe saad 10 tasuta'),
-(8, 'Pls work', 3, 'kapp2.jpg', '45.00', 5, 'I beg pls work'),
-(9, 'Viiuldav Riiul', 2, 'laud.jpg', '12.99', 2, 'Laulab sulle laulu');
+(8, 'Pls work', 1, 'kapp2.jpg', '45.00', 5, 'I beg pls work'),
+(9, 'Viiuldav Riiul', 2, 'laud.jpg', '12.99', 2, 'Laulab sulle laulu'),
+(10, 'ORMiga sisestatud asi', 3, '', '39.99', 3, 'Tore asi');
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `product_types`
+-- Tabeli struktuur tabelile `productthumb`
 --
 
-CREATE TABLE `product_types` (
+CREATE TABLE `productthumb` (
+  `id` int(11) NOT NULL,
+  `image_path` varchar(191) NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `productthumb`
+--
+
+INSERT INTO `productthumb` (`id`, `image_path`, `product_id`) VALUES
+(1, 'kapp2.jpg', 2),
+(2, 'Riiul1.jpg', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `producttype`
+--
+
+CREATE TABLE `producttype` (
   `product_type_id` tinyint(4) NOT NULL,
   `product_type` varchar(191) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Andmete tõmmistamine tabelile `product_types`
+-- Andmete tõmmistamine tabelile `producttype`
 --
 
-INSERT INTO producttype (`product_type_id`, `product_type`) VALUES
+INSERT INTO `producttype` (`product_type_id`, `product_type`) VALUES
 (1, 'Furniture'),
 (2, 'Woodturning'),
 (3, 'Sketchbooks');
@@ -117,15 +137,22 @@ INSERT INTO `users` (`user_id`, `user_name`, `is_admin`, `password`, `email`, `d
 --
 
 --
--- Indeksid tabelile `products`
+-- Indeksid tabelile `product`
 --
-ALTER TABLE product
-  ADD PRIMARY KEY (id);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indeksid tabelile `product_types`
+-- Indeksid tabelile `productthumb`
 --
-ALTER TABLE producttype
+ALTER TABLE `productthumb`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_thumbs_products_product_id_fk` (`product_id`);
+
+--
+-- Indeksid tabelile `producttype`
+--
+ALTER TABLE `producttype`
   ADD PRIMARY KEY (`product_type_id`);
 
 --
@@ -147,14 +174,19 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT tabelile `products`
+-- AUTO_INCREMENT tabelile `product`
 --
-ALTER TABLE product
-  MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Autocreated', AUTO_INCREMENT=10;
+ALTER TABLE `product`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Autocreated', AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT tabelile `product_types`
+-- AUTO_INCREMENT tabelile `productthumb`
 --
-ALTER TABLE producttype
+ALTER TABLE `productthumb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT tabelile `producttype`
+--
+ALTER TABLE `producttype`
   MODIFY `product_type_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT tabelile `translations`
@@ -166,6 +198,16 @@ ALTER TABLE `translations`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `productthumb`
+--
+ALTER TABLE `productthumb`
+  ADD CONSTRAINT `product_thumbs_products_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
